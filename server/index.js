c4de564f3de4e28a808a62a111f03fa419f1d222
briantam23 <briantam23@gmail.com>
2019-06-24 19:48:36 -0400

    Created route/middleware to serve index.html

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
+
+
+app.use('/public', express.static(path.join(__dirname, '../public')))
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 
 
 app.listen(PORT, () => console.log(`
