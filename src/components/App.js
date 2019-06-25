1cf303dfae83240f396d9ea70105bd3e4303e4d0
briantam23 <briantam23@gmail.com>
2019-06-24 20:53:52 -0400

    Created Webpack & React App component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+    render() {
+        return(
+            <h1>Brian Tam's Clothing App</h1>
+        )
+    }
+}
+
+
+export default App;
